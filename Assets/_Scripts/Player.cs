94e5154efaf93200c7c8c4f149c8f0c1eaf973d3
Raza Toosy <raza@toosy.net>
2022-06-04 18:26:49 +0100

    feat (Mouse Control) : added Mouse Control (depressed left click)

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,11 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float turnSpeed = 1.0f;
     [SerializeField] private float respawnTime = 3.0f;
     [SerializeField] private float respawnInvulnerabilityTime = 3.0f;
+    [SerializeField] private float rotationMomentum = 5.0f;
+    [SerializeField] private float turnDirection;
+    [SerializeField] private float angle;
     private bool _thrusting;
-    private float _turnDirection;
     private Rigidbody2D _rigidbody;
 
     public Action OnRespawn;
+
+    private Vector2 lastClickedPos;
+    private bool _isMoving;
 
     private void Awake()
     {
@@ -26,37 +32,88 @@ public class Player : MonoBehaviour
     private void Update()
     {
         _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        
+        
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            _turnDirection = 1.0f;
+            turnDirection = 1.0f;
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            _turnDirection = -1.0f;
+            turnDirection = -1.0f;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            //2 options
+            //Both do the same!
+            
+            //To use mouse location for the player to goto
+            //GetPlayerToGoToMouseLocation();
+            
+            //To use with moving in direction of the mouse
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _isMoving = true;
+            ControlWithMouse();
         }
         else
         {
-            _turnDirection = 0.0f;
+            turnDirection = 0.0f;
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
         }
+
     }
 
     private void FixedUpdate()  //Physics goes here
     {
-        if (_thrusting)
+        if (_thrusting || Input.GetMouseButton(0))
         {
             _rigidbody.AddForce(this.transform.up * thrustSpeed);
         }
 
-        if (_turnDirection != 0.0f)
+        if (turnDirection != 0.0f)
         {
-            _rigidbody.AddTorque(_turnDirection * turnSpeed);
+            _rigidbody.AddTorque(turnDirection * turnSpeed);
         }
     }
+
+    /// <summary>
+    /// Makes the player move to the point in the 2d world. Left click and keep depressed to have the player follow
+    /// the cursor
+    /// </summary>
+    private void GetPlayerToGoToMouseLocation()
+    {
+        var mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var playerdirection = transform.up.normalized;
+        var positionFromMouseToPlayer = mousePositionInWorld - transform.position;
+        if (Vector3.Angle(transform.right, positionFromMouseToPlayer - playerdirection) > 90f) turnDirection = 1.0f;
+        else turnDirection = -1.0f;
+        Debug.DrawRay(transform.position, transform.up.normalized * 100, Color.green);
+        Debug.DrawRay(transform.position, positionFromMouseToPlayer, Color.red);
+    }
+
+    /// <summary>
+    /// Makes the player move to the point in the 2d world. Left click and keep depressed to have the player follow
+    /// the cursor
+    /// </summary>
+    private void ControlWithMouse() // For click on point and rotate and move player in this direction
+    {
+        if (_isMoving && (Vector2) transform.position != lastClickedPos)
+        {
+            float step = thrustSpeed * Time.deltaTime;
+            transform.position = Vector2.Lerp(transform.position, lastClickedPos, step);
+
+            var direction = lastClickedPos - (Vector2)transform.position;
+
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, angleAxis, Time.deltaTime * rotationMomentum);
+        }else _isMoving = false;
+    }
+    
 
     private void Shoot()
     {
